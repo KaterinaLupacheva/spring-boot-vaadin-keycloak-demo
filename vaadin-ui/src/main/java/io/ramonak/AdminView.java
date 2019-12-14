07861f38bc76a42570ccdc4239193560fb66022f
Katerina <katerinalupacheva@gmail.com>
2019-12-14 15:52:04 +0300

    AdminView and MainView updated

--- vaadin-ui/src/main/java/io/ramonak/AdminView.java
+++ vaadin-ui/src/main/java/io/ramonak/AdminView.java
@@ -1,10 +1,7 @@
 package io.ramonak;
 
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
--- vaadin-ui/src/main/java/io/ramonak/MainView.java
+++ vaadin-ui/src/main/java/io/ramonak/MainView.java
@@ -1,16 +1,8 @@
 package io.ramonak;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.notification.Notification;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Route
 @PWA(name = "Demo Spring Boot - Vaadin - Keycloak", shortName = "Demo project")
